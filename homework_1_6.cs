9365e881134f1ddb4a904719dfb707a7f89da4fc
BSavova7 <bsfizika81@gmail.com>
2015-01-12 05:01:01 -0800

    Create homework_1_6.cs

---
+++ homework_1_6.cs
@@ -0,0 +1,13 @@
+using System;
+
+    class PrintNumbers
+    {
+        static void Main()
+        {           
+                Console.WriteLine("1");
+                Console.WriteLine("101");
+                Console.WriteLine("1001");
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine();   
+        }
+    }
