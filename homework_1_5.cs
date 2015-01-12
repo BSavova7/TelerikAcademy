27c441f1fda1fb9c9958c39bc10aa2261c35ac4a
BSavova7 <bsfizika81@gmail.com>
2015-01-12 04:59:31 -0800

    Create homework_1_5.cs

---
+++ homework_1_5.cs
@@ -0,0 +1,12 @@
+using System;
+
+    class PrintMyName
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Botka Savova");
+            Console.WriteLine("");
+            
+        }
+    }
+    
