fe5c1dcac3e962ee8c07e05cfb200d773b8d3dee
BSavova7 <bsfizika81@gmail.com>
2015-01-12 05:08:30 -0800

    Create homework_1_16.cs

---
+++ homework_1_16.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Squares
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Squares:");
+            Console.WriteLine();
+            for (int i = 2; i <1012; i++)
+            {
+               Console.Write("  ");
+               Console.Write(i*Math.Pow(-1,i));              
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
