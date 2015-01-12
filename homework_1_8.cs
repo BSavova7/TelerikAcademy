c804d97e81bae0f3ae5806e0cb6b5cf2a1af8ab8
BSavova7 <bsfizika81@gmail.com>
2015-01-12 05:04:00 -0800

    Create homework_1_8.cs

---
+++ homework_1_8.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class SquareRout
+    {
+        static void Main(string[] args)
+        {
+            int p;
+            p = int.Parse("12345");
+            Console.WriteLine("p=12345");
+            Console.WriteLine();
+            Console.Write("Sqrt p =  ");
+            Console.WriteLine(Math.Sqrt(p));
+            
+        }
+    }
