0b71629f23dd8aef25ea5353c665bcf9b26545f6
BSavova7 <bsfizika81@gmail.com>
2015-01-12 05:06:14 -0800

    Create homework_1_10.cs

---
+++ homework_1_10.cs
@@ -0,0 +1,14 @@
+using System;
+
+class hoRRiblEcoDe
+{
+     static void Main ()
+     {
+         Console.WriteLine ("Здравейте, аз съм ужасно форматирана програма");
+         Console.WriteLine ("Числа и квадрати:");
+         for (int I = 0;I <10;I ++)
+         {
+             Console.WriteLine (I + "->" + I *I);
+         }
+     }
+}
