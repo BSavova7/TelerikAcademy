f6659d4285e98122f88d27bc52aa630952a14936
BSavova7 <bsfizika81@gmail.com>
2015-01-12 04:53:57 -0800

    Create homework_1_1.cs

---
+++ homework_1_1.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace ConsoleApplication1
+{
+    class HelloWord
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#");
+            Console.WriteLine();
+        }
+    }
+}
