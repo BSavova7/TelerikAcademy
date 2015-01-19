f62aabe5d986761d65d47806a47a9e7247e9b2d4
BSavova7 <bsfizika81@gmail.com>
2015-01-19 20:59:22 +0100

    Create homework2_4.cs

---
+++ homework2_4.cs
@@ -0,0 +1,13 @@
+using System;
+
+    class CharacterVariable 
+    {
+        static void Main(string[] args)
+        {
+            char symbol;
+            symbol = (char) 42;
+            ushort hex = 0x2A;
+            Console.WriteLine(symbol);
+            Console.WriteLine(hex);
+        }
+    }
