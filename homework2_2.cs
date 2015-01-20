0188959fd2720f06082c0aa4832d85a8a1a17512
BSavova7 <bsfizika81@gmail.com>
2015-01-20 09:11:17 +0200

    Create homework2_2.cs

---
+++ homework2_2.cs
@@ -0,0 +1,17 @@
+using System;
+
+    class DoubleAndFloat
+    {
+        static void Main(string[] args)
+        {
+            Double a=34.567839023;
+            Double b=8923.1234857;
+            float c=12.345f;
+            float d=3456.091f;
+            Console.WriteLine("a= {0}    b= {1}   ",a,b);
+            Console.WriteLine();
+            Console.WriteLine("c= {0}    d={1}   ",c, d);
+            Console.WriteLine();
+        }
+    }
+
