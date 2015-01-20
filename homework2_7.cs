007a52261d7e6c314b7fce5089f05651dce2e878
BSavova7 <bsfizika81@gmail.com>
2015-01-20 09:14:48 +0200

    Create homework2_7.cs

---
+++ homework2_7.cs
@@ -0,0 +1,17 @@
+using System;
+
+class two_strings
+{
+    static void Main()
+    {
+        String MyName = "Botka";
+        String MyFamily = "Savova";
+       // Object Names;
+        String MyNames;
+        MyNames= MyName + "  " + MyFamily;
+        Console.WriteLine("{0} {1} {2} ", MyName, "  ", MyFamily);
+        Console.WriteLine();
+        Console.WriteLine(MyNames);
+        Console.WriteLine();
+    }
+}
