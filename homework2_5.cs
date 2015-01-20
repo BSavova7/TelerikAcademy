35cabb01611dd8079e975e5cec21eff47130db31
BSavova7 <bsfizika81@gmail.com>
2015-01-20 09:13:01 +0200

    Create homework2_5.cs

---
+++ homework2_5.cs
@@ -0,0 +1,16 @@
+using System;
+
+    class female
+    {
+        
+        static void Main()
+        {
+            Boolean isFemale; 
+            String gender;
+            gender = Console.ReadLine();
+            Console.WriteLine();
+            isFemale=(gender == "female");
+            Console.WriteLine(isFemale);
+            Console.WriteLine();
+        }
+    }
