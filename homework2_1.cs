f845d7d00d1c6a6c22a7361506773d1cf6c46f80
BSavova7 <bsfizika81@gmail.com>
2015-01-20 09:09:45 +0200

    Create homework2_1.cs

---
+++ homework2_1.cs
@@ -0,0 +1,17 @@
+using System;
+
+    class DataTypes
+    {
+        static void Main(string[] args)
+        { 
+            sbyte a=-115;
+            byte b=97;
+            ushort c=52130;
+            short d=-10000;
+            int h=4825932;
+            Console.WriteLine("a=  " + a + "    b= " + b + "    c= " + c);
+            Console.WriteLine();
+            Console.WriteLine("d=  " + d + "  h= " + h);
+            Console.WriteLine();
+        }
+    }
