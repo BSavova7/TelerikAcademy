1e2d6d9c166d66eb365a32f8ac321385300b667a
BSavova7 <bsfizika81@gmail.com>
2015-01-20 09:13:43 +0200

    Create homework2_6.cs

---
+++ homework2_6.cs
@@ -0,0 +1,19 @@
+using System;
+
+    class strings
+    {
+        static void Main()
+        {
+            String hello="Hello";
+            String world="World";
+            String helloworld = "";
+            Object typeCasting;
+            typeCasting=hello+ "  " + world;
+           // helloworld = typeCasting;
+            Console.WriteLine("{0} {1} ", hello, world);
+            Console.WriteLine(helloworld);
+            Console.WriteLine(typeCasting);
+
+}
+        }
+        
